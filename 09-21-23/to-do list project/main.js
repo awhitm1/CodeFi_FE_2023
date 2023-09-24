e924e0397a188b1176ec054b6ead961ca4943f3c
awhitm1 <41929321+awhitm1@users.noreply.github.com>
2023-09-23 23:17:02 -0500

    Functioning ToDo List - a few items to polish

--- 09-21-23/to-do list project/main.js
+++ 09-21-23/to-do list project/main.js
@@ -9,53 +9,53 @@ const muchToDoAboutNothing = [];
 const ul = document.querySelector('ul');
 let checkboxes=[];
 
-
-
-
-
-
-
-
-
 // Event Listeners
 input.addEventListener('focusin', focusInput);
-input.addEventListener('focusout',endFocus);
-ul.addEventListener('change', event => {
-  if (event.target.type === 'checkbox') {
-    const checked = document.querySelectorAll('input[type="checkbox"]:checked')
-    selected = Array.from(checked).map(x => x.value)
-    console.log(selected);
-    // selected.forEach(isChecked)
-  } 
-  
-});
-
-
+input.addEventListener('focusout', endFocus);
+overlay.addEventListener('click', endFocus);
 form.addEventListener('submit', createToDoItem);
-// checkboxes.addEventListener('change', isChecked);
 
+// Functions
 
 function createToDoItem (e){
   e.preventDefault();
-
+  // need to prevent empty items
   const toDo = input.value;
   const index = Math.floor(Math.random()*100000);
   const toDoContainer = document.createElement('li');
-  const newToDo = document.createElement('li');
+  const newToDo = document.createElement('label');
   const newCheckbox = document.createElement("INPUT");
+  const trashCan = document.createElement("i");
+  const trashCanButton = document.createElement("button");
+  
+  trashCanButton.setAttribute("class", "btn-trash");
+  trashCan.setAttribute("class", "todo-trash");
+  
   newCheckbox.setAttribute("type", "checkbox");
   newCheckbox.setAttribute("class", "checkbox");
-  newCheckbox.setAttribute("value",index);
+  newCheckbox.setAttribute("id",index);
   newCheckbox.setAttribute("name", index);
-
-  toDoContainer.setAttribute("id",index);
+  
+  newToDo.setAttribute("for", index);
+  newToDo.setAttribute("class", "strikeThrough");
   newToDo.innerText = toDo;
+  
+  trashCanButton.appendChild(trashCan);
+  newToDo.appendChild(trashCanButton);
   toDoContainer.appendChild(newCheckbox);
   toDoContainer.appendChild(newToDo);
-  
   todoList.appendChild(toDoContainer);
+
+  // Trash can event listener
+  trashCanButton.addEventListener('click', e => {
+    toDoContainer.parentNode.removeChild(toDoContainer)
+  });
+
+  // Saving to local storage
   muchToDoAboutNothing.push(toDo);
   saveMyToDos(muchToDoAboutNothing);
+
+
   form.reset();
 }
 
@@ -64,14 +64,13 @@ function focusInput(){
 }
 
 function endFocus(){
-  if (body.classList.contains('focus-form')) body.classList.remove('focus-Form');
+  if (body.classList.contains('focus-form')) body.classList.remove('focus-form');
 }
 
 function saveMyToDos(toDoS = []){
   localStorage.setItem('todo_list', JSON.stringify(toDoS));
 }
 
-function isChecked (index) {
-  
-  
-}
+
+
+
